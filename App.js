52b6ecf44ead21bed8d2f1b1166c5c5019124401
Black Arrow <137434164+BlackArrow220623@users.noreply.github.com>
2024-11-30 12:04:29 +0530

    Start Set up
    Start Set up

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text } from 'react-native';
+
+export default function App() {
+    return (
+      <Text style={{height:"100%",alignItems:"center"}}>
+        <Text style={styles.Text}>ShivLing</Text>
+      </Text>
+    );
+}
+
+const styles = StyleSheet.create({
+  Text:{
+    fontSize:25,
+    justifyContent:"center",
+    alignItems:"center",
+    alignContent:"center"
+  }
+});
+
