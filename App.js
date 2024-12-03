7c1b50cac90c34fa5eeade9bff234f4f14631276
Black Arrow <137434164+BlackArrow220623@users.noreply.github.com>
2024-12-03 15:36:47 +0530

    test

--- App.js
+++ App.js
@@ -3,15 +3,9 @@ import { StyleSheet, Text , View} from 'react-native';
 
 export default function App() {
     return (
-<<<<<<< Updated upstream
       <Text style={{height:"100%",alignItems:"center"}}>
         <Text style={styles.Text}>RACHANA</Text>
       </Text>
-=======
-      <View style={{backgroundColor:"#fff"}} >
-        <Text style={styles.Text}>ShivLing</Text>
-      </View>
->>>>>>> Stashed changes
     );
 }
 
