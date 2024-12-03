bb25325dfc70f2f561f6102fe094f4b5d54c4672
Black Arrow <137434164+BlackArrow220623@users.noreply.github.com>
2024-12-03 12:57:37 +0530

    Icon change Fix
    Icon change Fix

--- App.js
+++ App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import { StyleSheet, Text } from 'react-native';
+import { StyleSheet, Text , View} from 'react-native';
 
 export default function App() {
     return (
+<<<<<<< Updated upstream
       <Text style={{height:"100%",alignItems:"center"}}>
         <Text style={styles.Text}>RACHANA</Text>
       </Text>
+=======
+      <View style={{backgroundColor:"#fff"}} >
+        <Text style={styles.Text}>ShivLing</Text>
+      </View>
+>>>>>>> Stashed changes
     );
 }
 
